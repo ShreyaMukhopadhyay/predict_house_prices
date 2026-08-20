7d24bb38748471973aac8237e84019e7f167402e
Shreya Mukhopadhyay <mk.shreya@outlook.com>
2026-08-20 23:14:40 +0530

    updated clean

--- src/data_prep.py
+++ src/data_prep.py
@@ -6,41 +6,55 @@ from exception_handling import exception_handling
 from missing import missing_treatment
 from dummies import get_dummies
 
-train_df = load_data("train")
-data_description = load_data("data_dictionary")
+def preprocess(df):
+
+    df = load_data("train")
+    data_description = load_data("data_dictionary")
 
 
-# Define the id and dependent variable column names
-id_col = "id"
-dep_var = "saleprice"
+    # Define the id and dependent variable column names
+    id_col = "id"
+    dep_var = "saleprice"
 
-# One dtype per column
-column_dtypes = data_description.drop_duplicates("column_name").set_index("column_name")["dtype"]
+    # One dtype per column
+    column_dtypes = data_description.drop_duplicates("column_name").set_index("column_name")["dtype"]
 
-# List all numeric columns except id and dep_var
-num_vars = [
-    col
-    for col, dtype in column_dtypes.items()
-    if dtype != "object" and col not in [id_col, dep_var]
-]
-# List all categorical columns except id and dep_var
-cat_vars = [
-    col
-    for col, dtype in column_dtypes.items()
-    if dtype == "object" and col not in [id_col, dep_var]
-]
-
-
-# exception case handling
-train_df = exception_handling(train_df)
+    # List all numeric columns except id and dep_var
+    num_vars = [
+        col
+        for col, dtype in column_dtypes.items()
+        if dtype != "object" and col not in [id_col, dep_var]
+    ]
+    # List all categorical columns except id and dep_var
+    cat_vars = [
+        col
+        for col, dtype in column_dtypes.items()
+        if dtype == "object" and col not in [id_col, dep_var]
+    ]
 
 
-# missing treatment
-train_df = missing_treatment(df=train_df, num_vars=num_vars)
+    # exception case handling
+    df = exception_handling(df)
 
 
-# dummy creation
-train_df=get_dummies(df=train_df, cat_vars=cat_vars)
+    # missing treatment
+    df = missing_treatment(df=df, num_vars=num_vars)
 
-display(train_df.head())
+
+    # dummy creation
+    df=get_dummies(df=df, cat_vars=cat_vars)
+
+    return df
+
+
+if __name__=="__main__":
+    from load_data import load_data
+
+    train_df = load_data("train")
+    processed_train_df = preprocess(df=train_df)
+    print(processed_train_df.isnull().sum().sum())
+
+    test_df = load_data("test")
+    processed_test_df = preprocess(df=test_df)
+    print(processed_test_df.isnull().sum().sum())
 
