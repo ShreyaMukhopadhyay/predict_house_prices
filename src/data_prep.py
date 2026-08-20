df1e453606ad2450c0a26c3d76fa4f9c001e17ab
Shreya Mukhopadhyay <mk.shreya@outlook.com>
2026-08-20 11:10:20 +0530

    Add data_prep.py to classify numeric and categorical variables

---
+++ src/data_prep.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+from load_data import load_data
+
+train_df = load_data("train")
+data_description = load_data("data_dictionary")
+
+
+# Define the id and dependent variable column names
+id_col = "id"
+dep_var = "saleprice"
+
+# One dtype per column
+column_dtypes = data_description.drop_duplicates("column_name").set_index("column_name")["dtype"]
+
+# List all numeric columns except id and dep_var
+num_vars = [
+    col
+    for col, dtype in column_dtypes.items()
+    if dtype != "object" and col not in [id_col, dep_var]
+]
+# List all categorical columns except id and dep_var
+cat_vars = [
+    col
+    for col, dtype in column_dtypes.items()
+    if dtype == "object" and col not in [id_col, dep_var]
+]
